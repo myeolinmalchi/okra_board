9b6f1bacdc09cfbfc5560f620e503ee15eaf87ab
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-05-24 02:52:48 +0900

    delete CORSMiddleware

--- middleware/Middleware.go
+++ middleware/Middleware.go
@@ -1,8 +1,9 @@
 package middleware
 
 import (
-    "okra_board/services"
-    "github.com/gin-gonic/gin"
+	"fmt"
+	"okra_board/services"
+	"github.com/gin-gonic/gin"
 )
 
 func CORS() gin.HandlerFunc {
@@ -10,7 +11,8 @@ func CORS() gin.HandlerFunc {
         c.Header("Access-Control-Allow-Headers", "Content-Type, Autorization, Origin")
         c.Header("Access-Control-Allow-Credentials", "true")
         c.Header("Access-Control-Allow-Origin", "*")
-        c.Header("Access-Control-Allow-Methods", "GET, DELETE, POST, PUT")
+        c.Header("Access-Control-Allow-Methods", "GET, DELETE, POST, PUT, OPTIONS")
+        c.Header("Access-Control-Request-Methods", "GET, DELETE, POST, PUT, OPTIONS")
     }
 }
 
@@ -33,9 +35,76 @@ func Auth() gin.HandlerFunc {
     }
 }
 
+/*
+func Auth2() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        accessToken := c.Request.Header.Get("AccessToken")
+        if accessToken == "" {
+            c.JSON(401, gin.H {
+                "status": 401,
+                "message": "access token is empty",
+            })
+            c.Abort()
+        } else {
+            err, isExpired := services.VerifyAccessToken(accessToken)
+            if err != nil {
+                c.JSON(401, gin.H {
+                    "status": 401,
+                    "message": "invalid access token",
+                })
+                c.Abort()
+            } else if isExpired {
+                verifyRefreshToken(c)
+            }
+        }
+    }
+}
+
+func verifyRefreshToken(c *gin.Context) {
+    refreshToken := c.Request.Header.Get("RefreshToken")
+    if refreshToken == "" {
+        c.JSON(401, gin.H {
+            "status": 401,
+            "message": "refresh token is empty",
+        })
+        c.Abort()
+    } else {
+        err, isExpired := services.VerifyRefreshToken(refreshToken)
+        if err != nil {
+            c.JSON(401, gin.H {
+                "status": 401,
+                "message": "invalid refresh token",
+            })
+            c.Abort()
+        } else if isExpired {
+            
+        }
+    }
+}
+
+func verifyRefreshToken2(c *gin.Context) {
+    refreshToken := c.Request.Header.Get("RefreshToken")
+    if refreshToken == "" {
+        c.JSON(401, gin.H {
+            "status": 401,
+            "message": "refresh token is empty",
+        })
+        c.Abort()
+    } else {
+        err, claims := services.GetTokenClaims(refreshToken)
+        if err != nil {
+
+        } else {
+            id := claims["id"]
+        }
+    }
+}
+*/
+
 func IPWhiteList(whiteList map[string]bool) gin.HandlerFunc {
     return func(c *gin.Context) {
         if !whiteList[c.ClientIP()] {
+            fmt.Println("Permission denied: "+c.ClientIP())
             c.AbortWithStatusJSON(403, gin.H {
                 "status": 403,
                 "message": "Permission denied",
